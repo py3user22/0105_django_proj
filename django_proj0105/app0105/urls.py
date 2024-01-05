2206b80b1855148f7e7f5168e833f2acba79095b
JesusG <py3user22@github.com>
2024-01-05 15:47:28 -0600

    0105 added views and urls.py more notes

--- django_proj0105/app0105/urls.py
+++ django_proj0105/app0105/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home),
     path('homepage', views.home),
     path('homepage0105.html', views.home),
+    path('0105_mysql_demo3.html', views.mysql),
+    path('mysql_demo3', views.mysql),
 ]
--- django_proj0105/app0105/views.py
+++ django_proj0105/app0105/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'homepage0105.html', {})
 
+def mysql(request):
+    return render(request, '0105_mysql_demo3.html', {})
+
 
