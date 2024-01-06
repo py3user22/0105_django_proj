390adddb9b369c86e5bc5156e8b3b6f26ed116b4
JesusG <py3user22@github.com>
2024-01-05 21:41:59 -0600

    0105 drf html notes add

--- django_proj0105/app0105/admin.py
+++ django_proj0105/app0105/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Solar
+from .models import Solar, MenuItem
 
 
 admin.site.register(Solar)
+admin.site.register(MenuItem)
--- django_proj0105/app0105/models.py
+++ django_proj0105/app0105/models.py
@@ -7,4 +7,8 @@ class Solar(models.Model):
     description = models.TextField(max_length=255)
     add_desc = models.CharField(max_length=155)
 
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
 
